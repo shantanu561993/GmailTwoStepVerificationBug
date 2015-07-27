d7fe4f05f700600e4201c28946b4b3e5821988a2
Shantanu Khandelwal <shantanu561993@gmail.com>
2015-07-27 22:06:27 +0530

    implementation of bug

--- GmailTwoStepVerificationBug/GmailTwoStepVerificationBug.py
+++ GmailTwoStepVerificationBug/GmailTwoStepVerificationBug.py
@@ -1,1 +1,22 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
+import smtplib
+
+def check_email(username,password,smtp_host='smtp.gmail.com',smtp_port=587):
+    if type(username) != str or type(password) != str :
+        raise Exception("Username / Password must be of type String")
+    server = smtplib.SMTP()
+    server.connect(smtp_host,smtp_port)
+    server.ehlo()
+    server.starttls()
+    try:
+        server.login(username,password)
+        print("Two step Verification not detected")
+    except smtplib.SMTPAuthenticationError, msg:
+        if "Application-specific password required" in msg[1]:
+            print("Two Step Verification Detected ")
+        elif "Username and Password not accepted" in msg[1]:
+            print("Incorrect Username or Password , Please provide correct username and password to test the account. ")
+        else:
+            print("some unknown error.."+msg[1]) 
+            
--- GmailTwoStepVerificationBug/__init__.py
+++ GmailTwoStepVerificationBug/__init__.py
@@ -3,3 +3,5 @@
 __author__ = 'Shantanu Khandelwal'
 __email__ = 'shantanu561993@gmail.com'
 __version__ = '1.0'
+
+from .GmailTwoStepVerificationBug import check_email
